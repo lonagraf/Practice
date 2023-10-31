802c182f44464ff547a6ac46a0f1520d9a488983
lonagraf <lonagraf@mail.ru>
2023-11-01 04:15:50 +0700

    добавление, удаление клиентов

--- Practice/AddWindow.axaml.cs
+++ Practice/AddWindow.axaml.cs
@@ -1,13 +1,105 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+using MySql.Data.MySqlClient;
 
 namespace Practice;
 
 public partial class AddWindow : Window
 {
+    private Database _database = new Database();
     public AddWindow()
     {
+        Width = 400;
+        Height = 600;
         InitializeComponent();
+        LoadDataExperienceCmb();
+        LoadDataLevelCmb();
+    }
+
+    private void AddClientBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        try
+        {
+            _database.openConnection();
+            string sql =
+                "INSERT INTO Client (surname, firstname, birthday, phone_number, email, language_need, language_experience, language_level, Login, Password) " +
+                "VALUES (@Surname, @Name, @Birthday, @Phone, @Email, @Needs, @Experience, @Level, @Login, @Password)";
+            using(MySqlCommand command = new MySqlCommand(sql, _database.getConnection()))
+            {
+                command.Parameters.AddWithValue("@Surname", SurnameTxt.Text);
+                command.Parameters.AddWithValue("@Name", NameTxt.Text);
+                command.Parameters.AddWithValue("@Birthday", BirthdayPicker.SelectedDate.GetValueOrDefault());
+                command.Parameters.AddWithValue("@Phone", PhoneTxt.Text);
+                command.Parameters.AddWithValue("@Email", EmailTxt.Text);
+                command.Parameters.AddWithValue("@Needs", NeedTxt.Text);
+                int selectedExperienceId = GetSelectedExperienceId(ExperienceCmb.SelectedItem.ToString());
+                int selectedLevelId = GetSelectedLevelId(LevelCmb.SelectedItem.ToString());
+                command.Parameters.AddWithValue("@Experience", selectedExperienceId);
+                command.Parameters.AddWithValue("@Level", selectedLevelId);
+                command.Parameters.AddWithValue("@Login", LoginTxt.Text);
+                command.Parameters.AddWithValue("@Password", PassTxt.Text);
+                command.ExecuteNonQuery();
+                var box = MessageBoxManager.GetMessageBoxStandard("Успешно", "Успешно добавлен!", ButtonEnum.Ok);
+                var result = box.ShowAsync();
+            }
+            _database.closeConnection();
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+            throw;
+        }
+    }
+    private void LoadDataExperienceCmb()
+    {
+        _database.openConnection();
+        string sql =
+            "select experience_name from practice.language_experience;";
+        MySqlCommand command = new MySqlCommand(sql, _database.getConnection());
+        using (MySqlDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                ExperienceCmb.Items.Add(reader["experience_name"].ToString());
+            }
+        }
+        _database.closeConnection();
+    }
+    private void LoadDataLevelCmb()
+    {
+        _database.openConnection();
+        string sql =
+            "select level_name from practice.language_level;";
+        MySqlCommand command = new MySqlCommand(sql, _database.getConnection());
+        using (MySqlDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                LevelCmb.Items.Add(reader["level_name"].ToString());
+            }
+        }
+        _database.closeConnection();
+    }
+    private int GetSelectedExperienceId(string selectedExperience)
+    {
+        string sql = "SELECT language_experience_id FROM language_experience WHERE experience_name = @selectedExperience";
+        MySqlCommand command = new MySqlCommand(sql, _database.getConnection());
+        command.Parameters.AddWithValue("@selectedExperience", selectedExperience);
+        int selectedId = Convert.ToInt32(command.ExecuteScalar());
+        return selectedId;
+    }
+    
+    private int GetSelectedLevelId(string selectedLevel)
+    {
+        string sql = "SELECT language_level_id FROM language_level WHERE level_name = @selectedLevel";
+        MySqlCommand command = new MySqlCommand(sql, _database.getConnection());
+        command.Parameters.AddWithValue("@selectedLevel", selectedLevel);
+        int selectedId = Convert.ToInt32(command.ExecuteScalar());
+        return selectedId;
     }
 }
--- Practice/ClientWindow.axaml.cs
+++ Practice/ClientWindow.axaml.cs
@@ -1,8 +1,13 @@
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 using MySql.Data.MySqlClient;
 using Practice.Model;
 
@@ -14,7 +19,7 @@ public partial class ClientWindow : UserControl
     private List<Client> _clients;
 
     public string fullTable =
-        "select surname, firstname, birthday, phone_number, email, language_need, experience_name, level_name from practice.client " +
+        "select client_id, surname, firstname, birthday, phone_number, email, language_need, experience_name, level_name from practice.client " +
         "join practice.language_experience on client.language_experience = language_experience.language_experience_id " +
         "join practice.language_level on client.language_level = language_level.language_level_id;";
     public ClientWindow()
@@ -34,6 +39,7 @@ public partial class ClientWindow : UserControl
         {
             var currentClient = new Client()
             {
+                ClientID = reader.GetInt32("client_id"),
                 Surname = reader.GetString("surname"),
                 Firstname = reader.GetString("firstname"),
                 Birthday = reader.GetDateTime("birthday"),
@@ -47,5 +53,65 @@ public partial class ClientWindow : UserControl
         }
         _database.closeConnection();
         ClientDataGrid.ItemsSource = _clients;
+    }
+
+    private void AddBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        AddWindow addWindow = new AddWindow();
+        addWindow.Show();
+    }
+
+    public void Delete(int id)
+    {
+        try
+        {
+            _database.openConnection();
+            string sql = "delete from practice.client where client_id = @clientId;";
+            MySqlCommand command = new MySqlCommand(sql, _database.getConnection());
+            command.Parameters.AddWithValue("@clientId", id);
+            command.ExecuteNonQuery();
+            _database.closeConnection();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка при удалении: " + ex.Message);
+            throw;
+        }
+    }
+
+    private async void DeleteBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        try
+        {
+            Client selectedClient = ClientDataGrid.SelectedItem as Client;
+
+            if (selectedClient != null)
+            {
+                var warning = MessageBoxManager.GetMessageBoxStandard("Предупреждение", "Вы уверены что хотите удалить клиента?", ButtonEnum.YesNo);
+                var result = await warning.ShowAsync();
+                if (result == ButtonResult.Yes)
+                {
+                    Delete(selectedClient.ClientID);
+                    ShowTable(fullTable);
+                    var box = MessageBoxManager.GetMessageBoxStandard("Успешно", "Клиент успешно удален!", ButtonEnum.Ok);
+                    var successResult = box.ShowAsync();
+                }
+                else
+                {
+                    var cancelBox = MessageBoxManager.GetMessageBoxStandard("Отмена", "Операция удаления отменена", ButtonEnum.Ok);
+                    var cancelResult = cancelBox.ShowAsync();
+                }
+            }
+            else
+            {
+                var box = MessageBoxManager.GetMessageBoxStandard("Ошибка", "Выберите клиента для удаления!", ButtonEnum.Ok);
+                var result = box.ShowAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка при удалении: " + ex.Message);
+            throw;
+        }
     }
 }
